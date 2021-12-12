74d1f65be41615e10985649f43ea2c6434e6a8aa
Mujtaba <20193778@stu.uob.edu.bh>
2021-12-12 12:57:23 +0300

    requesting a 100 records

--- Functions.py
+++ Functions.py
@@ -4,6 +4,7 @@ BufferSize = 4096
 Encoding = 'ascii'
 import time
 
+access_key = '843591f5114e7c7994823d002e7b6013'
 limit = 5
 
 def send_records (socket, option, json_response):
@@ -12,142 +13,187 @@ def send_records (socket, option, json_response):
 
     if option == 1:
 
+        print('  List of  all arrived flights')
+
+        No_records = True
+
         for records in json_response['data']:
 
-            socket.send(records["flight"]["iata"].encode(Encoding))
-            time.sleep(0.01)
+            if records["flight_status"] == "landed":
 
-            try:
-                socket.send(str(records["departure"]["airport"]).encode(Encoding))
-                time.sleep(0.01)
-            except Exception:
-                socket.send(str(records["arrival"]["airport"]).encode(Encoding))
+                No_records = False
+
+                socket.send(records["flight"]["iata"].encode(Encoding))
                 time.sleep(0.01)
 
-            socket.send(records["arrival"]["actual"].encode(Encoding))
-            time.sleep(0.01)
+                try:
+                    socket.send(str(records["departure"]["airport"]).encode(Encoding))
+                    time.sleep(0.01)
+                except Exception:
+                    socket.send(str(records["arrival"]["airport"]).encode(Encoding))
+                    time.sleep(0.01)
 
-            socket.send(str(records["arrival"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(records["arrival"]["actual"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["arrival"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["arrival"]["terminal"]).encode(Encoding))
+                time.sleep(0.01)
 
-
-
+                socket.send(str(records["arrival"]["gate"]).encode(Encoding))
+                time.sleep(0.01)
         print('  records sent\n')
+        if No_records:
+            socket.send('no records'.encode(Encoding))
+        else:
+            socket.send('STOP'.encode(Encoding))
 
 
     elif option == 2:
 
+        print('  List of delayed flights')
+
+        No_records = True
+
         for records in json_response['data']:
-            socket.sendall(records["flight"]["iata"].encode(Encoding))
-            time.sleep(0.01)
 
-            socket.send(str(records["departure"]["airport"]).encode(Encoding))
-            time.sleep(0.01)
+            if records["arrival"]["delay"] is not None:
+                No_records = False
 
-            socket.send(str(records["departure"]["actual"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.sendall(records["flight"]["iata"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["departure"]["airport"]).encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["departure"]["actual"]).encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["departure"]["terminal"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["departure"]["gate"]).encode(Encoding))
+                time.sleep(0.01)
         print('  records sent\n')
+        if No_records:
+            socket.send('no records'.encode(Encoding))
+        else:
+            socket.send('STOP'.encode(Encoding))
 
 
     elif option == 3:
 
+        country_name = socket.recv(BufferSize).decode(Encoding)
+
+        print('  All flights arriving from', country_name)
+
+        country_parameters = {
+            'access_key': access_key,
+            'limit': 1
+        }
+        city_api_response = requests.get('http://api.aviationstack.com/v1/airports', country_parameters)
+        if city_api_response.status_code != 200:
+            print('!' * 5, 'Request error')
+            return
+
+        json_result = city_api_response.json()
+
+        country_airport = ' '
+
+        for name in json_result['data']:
+            if name["country_name"] == country_name:
+                country_airport = name["airline_name"]
+
+        No_records = True
+
         for records in json_response['data']:
-            socket.sendall(records["flight"]["iata"].encode(Encoding))
-            time.sleep(0.01)
 
-            socket.send(records["departure"]["airport"].encode(Encoding))
-            time.sleep(0.01)
+            if records["departure"]["airport"] == country_airport:
+                No_records = False
 
-            socket.send(str(records["departure"]["actual"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.sendall(records["flight"]["iata"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(records["departure"]["airport"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["departure"]["actual"]).encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["departure"]["terminal"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["departure"]["gate"]).encode(Encoding))
+                time.sleep(0.01)
         print('  records sent\n')
+        if No_records:
+            socket.send('no records'.encode(Encoding))
+        else:
+            socket.send('STOP'.encode(Encoding))
 
     elif option == 4:
-        for records in json_response['data']:
-            socket.sendall(records["flight"]["iata"].encode(Encoding))
-            time.sleep(0.01)
 
-            socket.sendall(records["flight_date"].encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(records["departure"]["airport"].encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["departure"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["departure"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["airport"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["flight_status"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["departure"]["scheduled"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["scheduled"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
-            time.sleep(0.01)
-
-            socket.send(str(records["arrival"]["delay"]).encode(Encoding))
-            time.sleep(0.01)
-        print('  records sent\n')
-
-
-
-    elif option == 3:
+        flight_icao = socket.recv(BufferSize).decode(Encoding)
+        No_records = True
 
         for records in json_response['data']:
-            socket.sendall(records["flight"]["iata"].encode(Encoding))
-            time.sleep(0.01)
+            if records["flight"]["icao"]:
+                No_records = False
 
-            socket.send(records["departure"]["airport"].encode(Encoding))
-            time.sleep(0.01)
+                socket.sendall(records["flight"]["iata"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["actual"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.sendall(records["flight_date"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(records["departure"]["airport"].encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["terminal"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["departure"]["gate"]).encode(Encoding))
+                time.sleep(0.01)
 
-            socket.send(str(records["departure"]["gate"]).encode(Encoding))
-            time.sleep(0.01)
+                socket.send(str(records["departure"]["terminal"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["airport"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["terminal"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["gate"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["flight_status"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["departure"]["scheduled"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["scheduled"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["estimated"]).encode(Encoding))
+                time.sleep(0.01)
+
+                socket.send(str(records["arrival"]["delay"]).encode(Encoding))
+                time.sleep(0.01)
         print('  records sent\n')
+        if No_records:
+            socket.send('no records'.encode(Encoding))
+        else:
+            socket.send('STOP'.encode(Encoding))
+    else:
+        return 'quit'
+
+
+
+
 
 
 
@@ -156,9 +202,14 @@ def send_records (socket, option, json_response):
 def receiving_records (socket):
 
     i = 1
-    stop_point = 0
-    while stop_point<limit:
-        flight_code = socket.recv(BufferSize)
+
+    while True:
+        flight_code = socket.recv(BufferSize).decode(Encoding)
+        if flight_code == 'no records':
+            print('== No arrived flight')
+            break
+        elif flight_code == 'STOP':
+            break
         airport = socket.recv(BufferSize)
         arrival = socket.recv(BufferSize)
         terminal = socket.recv(BufferSize)
@@ -169,26 +220,35 @@ def receiving_records (socket):
 
         print('='*15, 'flight', i, '='*15)
 
-        print('  flight code:',flight_code.decode(Encoding),
+        print('  flight code:',flight_code,
               '\n  airport:',airport.decode(Encoding),
               '\n  arrival time:',arrival.decode(Encoding),
               '\n  terminal:',terminal.decode(Encoding),
               '\n  gate:',gate.decode(Encoding))
         i+=1
-        stop_point+=1
+
 
 
 
 def recv_flights(socket):
 
     i =1
-    stop_point =0
-    while stop_point<limit:
+    while True:
         flight_code = socket.recv(BufferSize).decode(Encoding)
+        if flight_code == 'no records':
+            print('== No flight records')
+            break
+        elif flight_code == 'STOP':
+            break
+
         airport = socket.recv(BufferSize).decode(Encoding)
+
         departure = socket.recv(BufferSize).decode(Encoding)
+
         arrival = socket.recv(BufferSize).decode(Encoding)
+
         terminal = socket.recv(BufferSize).decode(Encoding)
+
         gate = socket.recv(BufferSize).decode(Encoding)
 
 
@@ -201,38 +261,57 @@ def recv_flights(socket):
               '\n  terminal:', terminal,
               '\n  gate:', gate,'\n')
         i += 1
-        stop_point+=1
+
 
 
 def flight_details(socket):
-    flight_code = socket.recv(BufferSize).decode(Encoding)
-    date = socket.recv(BufferSize).decode(Encoding)
-    departure_airport = socket.recv(BufferSize).decode(Encoding)
-    departure_gate = socket.recv(BufferSize).decode(Encoding)
-    departure_terminal = socket.recv(BufferSize).decode(Encoding)
-    arrival_airport = socket.recv(BufferSize).decode(Encoding)
-    arrival_terminal = socket.recv(BufferSize).decode(Encoding)
-    arrival_gate = socket.recv(BufferSize).decode(Encoding)
-    flight_status = socket.recv(BufferSize).decode(Encoding)
-    scheduled_departure = socket.recv(BufferSize).decode(Encoding)
-    scheduled_arrival = socket.recv(BufferSize).decode(Encoding)
-    estimated_arrival = socket.recv(BufferSize).decode(Encoding)
-    delay = socket.recv(BufferSize).decode(Encoding)
 
-    print('=' * 15, 'flight', '=' * 15)
-    print('  Flight code:', flight_code,
-          '\n  Date:',date,
-          '\n  Departure airport:', departure_airport,
-          '\n  Departure gate:', departure_gate,
-          '\n  Departure terminal:',departure_terminal,
-          '\n  Arrival airport:', arrival_airport,
-          '\n  Arrival terminal:', arrival_terminal,
-          '\n  Arrival gate:', arrival_gate,
-          '\n  Flight status:',flight_status,
-          '\n  Scheduled departure:',scheduled_departure,
-          '\n  Scheduled arrival:',scheduled_arrival,
-          '\n  Estimated arrival:',estimated_arrival,
-          '\n  Delay:',delay,'\n')
+    while True:
+        flight_code = socket.recv(BufferSize).decode(Encoding)
+        if flight_code == 'no records':
+            print('== No arrived flight')
+            break
+        elif flight_code == 'STOP':
+            break
+
+        date = socket.recv(BufferSize).decode(Encoding)
+
+        departure_airport = socket.recv(BufferSize).decode(Encoding)
+
+        departure_gate = socket.recv(BufferSize).decode(Encoding)
+
+        departure_terminal = socket.recv(BufferSize).decode(Encoding)
+
+        arrival_airport = socket.recv(BufferSize).decode(Encoding)
+
+        arrival_terminal = socket.recv(BufferSize).decode(Encoding)
+
+        arrival_gate = socket.recv(BufferSize).decode(Encoding)
+
+        flight_status = socket.recv(BufferSize).decode(Encoding)
+
+        scheduled_departure = socket.recv(BufferSize).decode(Encoding)
+
+        scheduled_arrival = socket.recv(BufferSize).decode(Encoding)
+
+        estimated_arrival = socket.recv(BufferSize).decode(Encoding)
+
+        delay = socket.recv(BufferSize).decode(Encoding)
+
+        print('=' * 15, 'flight', '=' * 15)
+        print('  Flight code:', flight_code,
+              '\n  Date:',date,
+              '\n  Departure airport:', departure_airport,
+              '\n  Departure gate:', departure_gate,
+              '\n  Departure terminal:',departure_terminal,
+              '\n  Arrival airport:', arrival_airport,
+              '\n  Arrival terminal:', arrival_terminal,
+              '\n  Arrival gate:', arrival_gate,
+              '\n  Flight status:',flight_status,
+              '\n  Scheduled departure:',scheduled_departure,
+              '\n  Scheduled arrival:',scheduled_arrival,
+              '\n  Estimated arrival:',estimated_arrival,
+              '\n  Delay:',delay,'\n')
 
 
 
--- Server.py
+++ Server.py
@@ -2,7 +2,6 @@ import requests
 import json
 import socket
 import threading
-import time
 import Functions
 
 
@@ -23,110 +22,43 @@ def connection(socket, address):
     print('=' * 5, Name, 'is connected', '=' * 5, '\n')
 
 
-    parameters = {}
 
-    access_key = '3c20cacdeae406247164d1992c2a9fd4'
-    limit = 5
+
+    params = {
+        'access_key' : '843591f5114e7c7994823d002e7b6013',
+        'limit' : 100
+    }
+
+    with open('group_7.json', 'w') as file:
+
+        api_response = requests.get('http://api.aviationstack.com/v1/flights', params)
+        if api_response.status_code != 200:
+            print('!' * 5, 'Request error')
+
+        json.dump(api_response.json(), file, indent=3)
 
 
 
     option = 0
 
-
     while True:
-        with open('group_7.json', 'w') as file:
 
-            try:
-                option = int(socket.recv(BufferSize).decode(Encoding))
-            except Exception:
+        try:
+            option = int(socket.recv(BufferSize).decode(Encoding))
+        except Exception:
+            print('=' * 5, Name, 'has disconnected from the server', '=' * 5, '\n')
+            break
+
+
+        with open('group_7.json', 'r') as File:
+
+            jason_results = json.loads(File.read())
+            records_results = Functions.send_records(socket, option, jason_results)
+            if records_results == 'quit':
                 print('=' * 5, Name, 'has disconnected from the server', '=' * 5, '\n')
                 break
-
-            print('=' * 5, 'request received from', Name, '=' * 5, '\n')
-
-            if option == 1:
-                print('  List of  all arrived flights')
-
-                parameters = {
-                    'access_key': access_key,
-                    'limit': limit,
-                    'flight_status': 'landed'
-                }
-            elif option == 2:
-                print('  List of delayed flights')
-
-                parameters = {
-                    'access_key': access_key,
-                    'limit': limit,
-                    'min_delay_arr': 1
-                }
-
-            elif option == 3:
-                country_name = socket.recv(BufferSize).decode(Encoding)
-
-                print('  All flights arriving from',country_name)
-
-                country_parameters = {
-                    'access_key': access_key,
-                    'limit':1
-                }
-                city_api_response = requests.get('http://api.aviationstack.com/v1/airlines', country_parameters)
-                if city_api_response.status_code != 200:
-                    print('!' * 5, 'Request error')
-                    continue
-
-                json_result = city_api_response.json()
-
-                country_airline = ' '
-
-                for name in json_result['data']:
-                    if name["country_name"] == country_name:
-                        country_airline = name["airline_name"]
-
-                parameters = {
-                    'access_key': access_key,
-                    'limit': limit,
-                    'airline_name':country_airline
-                }
-
-
-                for name in json_result['data']:
-                    if name["country_name"] == country_name:
-                            country_airline = name["airline_name"]
-
-                    parameters = {
-                        'access_key': access_key,
-                        'limit': limit,
-                        'airline_name': country_airline
-                    }
-
-            elif option == 4:
-                flight_icao = socket.recv(BufferSize).decode(Encoding)
-                parameters = {
-                    'access_key': access_key,
-                    'limit': 1,
-                    'flight_icao': flight_icao
-                }
-
             else:
-                print('=' * 5, Name, 'has disconnected from the server', '=' * 5, '\n')
-                break
-
-            api_response = requests.get('http://api.aviationstack.com/v1/flights', parameters)
-            if api_response.status_code != 200:
-                print('!' * 5, 'Request error')
-                continue
-
-            json.dump(api_response.json(), file, indent=3)
-
-            file.close()
-
-            with open('group_7.json', 'r') as File:
-                jason_results = json.loads(File.read())
-                File.close()
-
-
-            Functions.send_records(socket, option, jason_results)
+                print('=' * 5, 'request received from', Name, '=' * 5, '\n')
 
 
 
